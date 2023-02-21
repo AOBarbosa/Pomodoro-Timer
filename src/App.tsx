b1ee3d182ad987f9066dd1ba2ce7c0e19da61cd9
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-02-21 17:42:09 -0300

    Testing some styled components props in the temporary button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
